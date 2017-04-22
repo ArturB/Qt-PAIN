487c4ea5b37fae5e6d9222bdd36a2055a7d2fbc4
ArturB <artur.brodzki@gmail.com>
2017-04-22 23:35:09 +0200

    initial commit

---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QTextStream>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_actionZamknij_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    void loadTextFile();
+};
+
+#endif // MAINWINDOW_H
